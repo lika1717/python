eb93e11990adb58f07398c828c214d5061ddeef9
anzhelika <anzhelika.skliar@nure.ua>
2020-05-25 20:44:10 +0300

    l

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+
+def landing(request):
+    name = "lika"
+    current_day = "03.01.2017"
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print (request.POST)
+        print (form.cleaned_data)
+        data  = form.cleaned_data
+        print (data["name"])
+
+        new_form = form.save()
+
+    return render(request, 'landing/landing.html', locals())
